c77e477da20931d6cd31fc32a93a7d108f219d52
Jzy <jestalycastillo15@gmail.com>
2024-09-16 10:22:47 +0800

    organize structures

--- DSAPLP3/ArrayOperations.java
+++ DSAPLP3/ArrayOperations.java
@@ -8,14 +8,21 @@ public class ArrayOperations {
     }
 
     public int insertElement() {
+        System.out.println("inserting element.");
         return 0;
     }
 
     public int searchElement() {
+        System.out.println("searching element.");
         return 0;
     }
 
+    public void displayArray() {
+        System.out.println("displaying array.");
+    }
+
     public int deleteElement() {
+        System.out.println("deleting element.");
         return 0;
     }
 
--- DSAPLP3/Main.java
+++ DSAPLP3/Main.java
@@ -1,9 +1,77 @@
 package DSAPLP3;
 
+import java.util.*;
+
 public class Main {
     public static void main(String args[]) {
+
+        Scanner scanner = new Scanner(System.in);
         ArrayOperations arrayOperations = new ArrayOperations();
 
-        arrayOperations.stopProgram();
+        boolean loop = true;
+
+        while (loop) {
+
+            System.out.println("---------MENU---------");
+            System.out.println("[1] CREATE ARRAY");
+            System.out.println("[2] INSERT ELEMENTS");
+            System.out.println("[3] SEARCH");
+            System.out.println("[4] DISPLAY");
+            System.out.println("[5] DELETE");
+            System.out.println("[0] STOP");
+
+            System.out.print("ENTER CHOICE: ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("----------------------");
+                    System.out.println();
+                    arrayOperations.createArray();
+                    System.out.println();
+
+                    break;
+                case 2:
+                    System.out.println("----------------------");
+                    System.out.println();
+                    arrayOperations.insertElement();
+                    System.out.println();
+                    break;
+                case 3:
+                    System.out.println("----------------------");
+                    System.out.println();
+                    arrayOperations.searchElement();
+                    System.out.println();
+                    break;
+                case 4:
+                    System.out.println("----------------------");
+                    System.out.println();
+                    arrayOperations.displayArray();
+                    System.out.println();
+                    break;
+                case 5:
+                    System.out.println("----------------------");
+                    System.out.println();
+                    arrayOperations.deleteElement();
+                    System.out.println();
+                    break;
+                case 0:
+                    System.out.println("----------------------");
+                    System.out.println();
+                    arrayOperations.stopProgram();
+                    System.out.println();
+                    System.out.println("----------------------");
+                    loop = false;
+                    break;
+                default:
+                    System.out.println("----------------------");
+                    System.out.println();
+                    System.out.println("ERROR. ENTER VALID INPUT.");
+                    System.out.println();
+
+            }
+        }
+        scanner.close();
     }
+
 }
